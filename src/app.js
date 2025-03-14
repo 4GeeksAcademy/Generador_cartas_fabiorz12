bf94a2bd1f2153f556efacb221d04cfc5b104b94
Fabio Rodríguez <fabioaro10@gmail.com>
2025-03-14 19:52:53 +0000

    comienzo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let cardNumber = [ "As","2","3","4","5","6","7","8","9","10","J","Q","K"];
+  let suit = ["Spades", "Diamonds","Hearts","Clubs"]
+  let randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+  let randomSuit = Math.floor(Math.random() * suit.length);
+  let finalSuit = suit[randomSuit]
+
+  
+}
